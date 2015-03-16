28dbdd287772401cacbe8f4b6b56bbc38c9f7ab8
Andriy <zubr.andr@gmail.com>
2015-03-16 19:33:57 +0100

    Final version of solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,55 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-# I've made first changes in document. Now I'll try to sync it with GitHub.
 
 ## Write a short comment describing this function
+
+# makeCacheMatrix creates a list of 4 functions: 
+# 2 to manipulate input matrix (set it and get it)
+# and 2 to manipulate cached value of inversed matrix
+# (get inversed matrix and set inversed matrix)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+# Function algorithm:
+# 1) checking if matrix is squared: standart deviation equals
+# 0 if both dimensions are equal; If matrix is equal, proceed
+# with next steps. In case of non-square matrix, show message.
+# 2) checking if cache variable contains inverse matrix. If 
+# contains - write appropriate message and skip step 3.
+# 3) calculating inversed matrix and putting it into cache 
+# variable.
+# 4) Return value of inversed matrix.
+
+cacheSolve <- function(x) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if (sd(dim(x$get()))=0) {
+        if (is.null(inv)) {
+            data <- x$get()
+            inv <- solve(data)
+            x$setinverse(inv)
+        } else {
+            message("getting cached data")
+        }
+        inv
+    } else {
+        message("matrix is not square")
+    }
 }
+
+
